919da11b0ec32108cd9e61eda90b9d8dbfe551d9
My Name <cpgsom819@gmail.com>
2023-03-28 03:46:32 +0800

    cart added

--- src/Components/AllGun/AllGun.js
+++ src/Components/AllGun/AllGun.js
@@ -15,21 +15,28 @@ const AllGun = () => {
 
 
     return (
+
+
         <div>
-            <h1 className='text-4xl text-center font-bold mt-4'> Welcome To Gun Store  </h1>
+                <h1 className='text-4xl text-center font-bold mt-4'> Welcome To Gun Store  </h1>
  
-            <p className='text-center'>Shop the best collection of new and used handguns online at Guns.com. Our ever-growing inventory includes all styles, calibers, and price ranges.</p>
+ <p className='text-center'>Shop the best collection of new and used handguns online at Guns.com. Our ever-growing inventory includes all styles, calibers, and price ranges.</p>
+
+        <div  className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10'>
+        
 
           
              {guns.map(( gun) => (
 
                    <SimpleGun
-                   gun={<i class="fa fa-graduation-cap" aria-hidden="true"></i>}
+                   gun={gun}
                    ></SimpleGun>
 
              ) )}
 
                    
+        </div>
+
         </div>
     );
 };
--- src/Components/AllGun/SimpleGun/SimpleGun.js
+++ src/Components/AllGun/SimpleGun/SimpleGun.js
@@ -2,10 +2,26 @@ import React from 'react';
 
 const SimpleGun = (gun) => {
 
-    console.log(gun)
+    const singlegun = gun.gun;
+
+    const {action, bullet, category, img,name, price } = singlegun;
+ 
     return (
         <div>
-            <h1> njnjn{gun.length}</h1>
+          <div className="card  bg-base-100 shadow-xl w-full">
+                    <figure className="px-10 pt-10">
+                        <img  src={img} alt="Shoes" className="rounded-xl w-96 h-40 " />
+                    </figure>
+
+                    <div className="card-body items-center text-center">
+                        <h2 className="card-title">{name}</h2>
+                        <p>If a dog chews shoes whose shoes does he choose?</p>
+                        <div className="card-actions">
+
+                        <button className="btn btn-primary">Buy Now</button>
+                        </div>
+                    </div>
+                </div>
         </div>
     );
 };
